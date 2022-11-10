7a3dda45c3645096b7f353d449a7939f87ea48fd
nuncaboi <109115409+nuncaboi@users.noreply.github.com>
2022-11-10 00:36:34 -0600

    cosas

--- Assets/Scripts/Camara.cs
+++ Assets/Scripts/Camara.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Camara : MonoBehaviour
 {
+    public float speed;
+    Vector3 _dir;
+    private Transform _myTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _myTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _dir = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        _myTransform.Translate = Time.deltaTime * speed * _dir;
     }
 }
