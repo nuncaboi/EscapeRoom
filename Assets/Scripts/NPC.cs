3186b8a5f1396a04c258fec49551837f43b62960
emiliosae <109813234+emiliosae@users.noreply.github.com>
2022-12-06 14:12:13 -0600

    fsdfsf

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public string Name;
+    public GameObject Range;
+    private bool _inRange;
+
+    private void Update()
+    {
+        if (_inRange /*& Input.GetButton("E")*/)
+        {
+            
+        }
+    }
+
+}
